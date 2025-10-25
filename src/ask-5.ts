da5dc3a5e6c7d3044a7fd230660202cb8004a87c
StanislavRakitianskiy <raketaswig@gmail.com>
2025-10-25 16:24:10 +0300

    success tasks

---
+++ src/ask-5.ts
@@ -0,0 +1,12 @@
+type StatusServer = "loading" | "success" | "error";
+function logStatus(status: StatusServer): void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
